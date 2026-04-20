525faff4aa8f931ac8ded2a5176d7acd5250953e
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-04-20 21:34:05 +0300

    ListBox DONE

---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		void Button_Click(object sender, EventArgs e)
+		{
+			switch ((sender as Button).Content)
+			{
+				case "ADD":
+					if (!string.IsNullOrWhiteSpace(txtInput.Text))
+					{
+						listBox.Items.Add(txtInput.Text);
+						txtInput.Text = "";
+					}
+					break;
+				case "CLR": listBox.Items.Clear(); break;
+				case "DEL":
+					if (listBox.SelectedIndex != -1) listBox.Items.RemoveAt(listBox.SelectedIndex);
+					break;
+			}
+		}
+	}
+}
